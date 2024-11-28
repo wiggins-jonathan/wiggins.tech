626656f01717b108df7f65d8a1599d7010360041
Jonathan Wiggins <jonathan@wiggins.tech>
2024-11-28 12:12:44 -0700

    change branch from 24.05 to unstable
    This gets us access to more modern versions of Go & Hugo

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Development environment with dependencies for wiggins.tech";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs }:
